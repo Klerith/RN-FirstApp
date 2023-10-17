8ebf1b60d1dc63f7d223cb4e1576119505cd6091
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-17 15:15:19 -0400

    App.tsx Formateado

--- App.tsx
+++ App.tsx
@@ -5,21 +5,21 @@ import { PaperProvider } from 'react-native-paper';
 import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
 
 
-import IonIcon from 'react-native-vector-icons/Ionicons'
+import IonIcon from 'react-native-vector-icons/Ionicons';
 
 
 export const App = () => {
   return (
     <PaperProvider
-      settings={{
-        icon: (props) => <IonIcon { ...props } />
-      }}
+      settings={ {
+        icon: ( props ) => <IonIcon { ...props } />
+      } }
     >
-      <SafeAreaView style={{ flex: 1}}>
-        {/* <HelloWorldScreen name='Fernando Herrera' /> */}
-        {/* <CounterScreen /> */}
+      <SafeAreaView style={ { flex: 1 } }>
+        {/* <HelloWorldScreen name='Fernando Herrera' /> */ }
+        {/* <CounterScreen /> */ }
         <CounterM3Screen />
       </SafeAreaView>
     </PaperProvider>
-  )
-}
+  );
+};
