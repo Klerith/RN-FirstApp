0db20cf93c9d2c0281a814cb4cdcb6ec906d647b
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-18 15:21:56 -0400

    Align Self

--- src/presentation/screens/FlexDirectionScreen.tsx
+++ src/presentation/screens/FlexDirectionScreen.tsx
@@ -3,10 +3,11 @@ import { StyleSheet, View } from 'react-native';
 export const FlexDirectionScreen = () => {
   return (
     <View style={ styles.container }>
-
+      
       <View style={[ styles.box, styles.box1 ]} />
       <View style={[ styles.box, styles.box2 ]} />
       <View style={[ styles.box, styles.box3 ]} />
+      <View style={[ styles.box, styles.box4 ]} />
 
     </View>
   );
@@ -18,22 +19,26 @@ const styles = StyleSheet.create( {
     flex: 1, 
     backgroundColor: '#d1d1d1',
     justifyContent: 'space-between',
-    alignItems: 'center',
+    alignItems: 'stretch',
     flexDirection: 'row'
   },
   box: {
+    // flex: 1,
     width: 100,
     height: 100,
   },
   box1: {
     backgroundColor: '#5856D6',
-    top: -100,
+    // top: -100,
   },
   box2: {
     backgroundColor: '#4240a2',
+    alignSelf: 'flex-end'
   },
   box3: {
     backgroundColor: '#2e2d71',
-    top: 100
+  },
+  box4: {
+    backgroundColor: '#222155',
   },
 } );
